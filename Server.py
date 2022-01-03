838793496dfd79a539ff6cad296c36915f63d575
coderLuan <zhoubo616819598@sina.com>
2022-01-03 15:43:59 +0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -10,17 +10,18 @@ logging.basicConfig(level=logging.INFO)
 app.config["MangaPath"] = Path("Y:\Manga")
 app.config["AVPath"] = Path("X:\\")
 
+
 @app.route("/", methods=["GET"])
 def api_main():
     logging.info("更新全部json")
     dict = {}
-    dict["actor"] = []
-    dict["av"] = []
-    for av in app.config["AVPath"].rglob('*.nfo'):
-        id = av.name.split(" ")[0]
-        dict["av"].append(id)
-    with open('av.json','w',encoding='utf8')as fp:
-        json.dump(dict,fp,ensure_ascii=False)
+    for actor in app.config["AVPath"].iterdir():
+        if actor.is_dir():
+            dict[actor.stem] = []
+            for video in actor.rglob("*.nfo"):
+                dict[actor.stem].append(video.stem.split(" ")[0])
+    with open("av.json", "w", encoding="utf8") as fp:
+        json.dump(dict, fp, ensure_ascii=False)
     logging.info("av.json done!")
 
     dict = {}
@@ -30,24 +31,27 @@ def api_main():
         if artist not in dict:
             dict[artist] = []
         dict[artist].append(name)
-    with open('manga.json','w',encoding='utf8')as fp:
-        json.dump(dict,fp,ensure_ascii=False)
+    with open("manga.json", "w", encoding="utf8") as fp:
+        json.dump(dict, fp, ensure_ascii=False)
     logging.info("manga.json done!")
 
     return "更新完成"
 
+
 @app.route("/manga", methods=["GET"])
 def api_manga():
-    with open('./manga.json','r',encoding='utf8')as fp:
+    with open("./manga.json", "r", encoding="utf8") as fp:
         dict = json.load(fp)
         return dict
 
 
 @app.route("/av", methods=["GET"])
 def api_av():
-    with open('./av.json','r',encoding='utf8')as fp:
+    with open("./av.json", "r", encoding="utf8") as fp:
         dict = json.load(fp)
         return dict
 
+
 if __name__ == "__main__":
+    # api_main()
     app.run(host="0.0.0.0", port="8864")
